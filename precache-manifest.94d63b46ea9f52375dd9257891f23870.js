64da88cbc6143d7b24fd38e93ce73706069271fd
Andrey Chupakhin <atom1543@yandex.ru>
2020-01-31 12:46:32 +0300

    Updates

---
+++ precache-manifest.94d63b46ea9f52375dd9257891f23870.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b6f6ec79220bfa72ca9c65efb1c3d35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b5235b18f44f24896f0",
+    "url": "/static/css/main.bf51a6ad.chunk.css"
+  },
+  {
+    "revision": "55d240bff3614e5db2b0",
+    "url": "/static/js/2.c6d9b535.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c6d9b535.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b5235b18f44f24896f0",
+    "url": "/static/js/main.34a9a146.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "ad226c69331f39d87e5c3db5699cf76e",
+    "url": "/static/media/profile.ad226c69.jpg"
+  }
+]);
