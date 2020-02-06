3a149e22ba73917d326fb69e18bdf3c87d0d638c
Andrey Chupakhin <atom1543@yandex.ru>
2020-02-06 17:07:03 +0300

    Modified profile info. Grouped icons.

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -18,41 +18,77 @@ class Landing extends Component {
           <h1 className="mb-0">{this.landingData.firstName}
             <span className="text-primary">{this.landingData.lastName}</span>
           </h1>
-          <div className="subheading mb-5">{this.landingData.phoneNumber} ·
+          <div className="subheading mb-5">
             <a href="mailto:name@email.com">{this.landingData.email}</a>
           </div>
           <p className="lead mb-5">{this.landingData.bio}</p>
-          <div className="social-icons">
-            <a href={this.landingData.linkedin}>
-              <FontAwesomeIcon icon={faLinkedin} />
-            </a>
-            <a href={this.landingData.github}>
-              <FontAwesomeIcon icon={faGithub} />
-            </a>
-            <a href={this.landingData.facebook}>
-              <FontAwesomeIcon icon={faFacebook} />
-            </a>
-            <a href={this.landingData.telegram}>
-              <FontAwesomeIcon icon={faTelegram} />
-            </a>
-            <a href={this.landingData.instagram}>
-              <FontAwesomeIcon icon={faInstagram} />
-            </a>
-            <a href={this.landingData.istina}>
-               <FontAwesomeIcon icon={faNewspaper} />
-            </a>
-            <a href={this.landingData.strava}>
-              <FontAwesomeIcon icon={faStrava} />
-            </a>
-            <a href={this.landingData.garmin}>
-              <FontAwesomeIcon icon={faCaretUp} />
-            </a>
-            <a href={this.landingData.scopus}>
-              <FontAwesomeIcon icon={faMicroscope} />
-            </a>
-            <a href={this.landingData.mendeley}>
-              <FontAwesomeIcon icon={faMendeley} />
-            </a>
+
+
+          <div className="container">
+            <div className="row">
+              <div className="social-icons col-sm">
+                <p className="lead mb-1">Work</p>
+                <div className="row mb-1">
+                <a href={this.landingData.linkedin}>
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+                </div>
+                <div className="row mb-1">
+                <a href={this.landingData.github}>
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+                </div>
+              </div>
+              <div className="social-icons col-sm">
+                <p className="lead mb-1">Contacts</p>
+                <div className="row mb-1">
+                <a href={this.landingData.facebook}>
+                  <FontAwesomeIcon icon={faFacebook} />
+                </a>
+                </div>
+                <div className="row mb-1">
+                <a href={this.landingData.telegram}>
+                  <FontAwesomeIcon icon={faTelegram} />
+                </a>
+                </div>
+                <div className="row mb-1">
+                <a href={this.landingData.instagram}>
+                  <FontAwesomeIcon icon={faInstagram} />
+                </a>
+                </div>
+              </div>
+              <div className="social-icons col-sm">
+                <p className="lead mb-1">Science</p>
+                <div className="row mb-1">
+                <a href={this.landingData.istina}>
+                   <FontAwesomeIcon icon={faNewspaper} />
+                </a>
+                </div>
+                <div className="row mb-1">
+                <a href={this.landingData.scopus}>
+                  <FontAwesomeIcon icon={faMicroscope} />
+                </a>
+                </div>
+                <div className="row mb-1">
+                <a href={this.landingData.mendeley}>
+                  <FontAwesomeIcon icon={faMendeley} />
+                </a>
+                </div>
+              </div>
+              <div className="social-icons col-sm">
+                <p className="lead mb-1">Sport</p>
+                <div className="row mb-1">
+                <a href={this.landingData.strava}>
+                  <FontAwesomeIcon icon={faStrava} />
+                </a>
+                </div>
+                <div className="row mb-1">
+                <a href={this.landingData.garmin}>
+                  <FontAwesomeIcon icon={faCaretUp} />
+                </a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
